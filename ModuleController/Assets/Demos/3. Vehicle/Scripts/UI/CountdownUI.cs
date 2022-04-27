589e064622a400503a2f9286da9101e682bf277e
Sokka06 <Sokka06@gmail.com>
2022-04-27 19:49:07 +0300

    baked new lightmap, added countdown background

--- ModuleController/Assets/Demos/3. Vehicle/Scripts/UI/CountdownUI.cs
+++ ModuleController/Assets/Demos/3. Vehicle/Scripts/UI/CountdownUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Demos.Vehicle
 {
@@ -22,12 +23,18 @@ namespace Demos.Vehicle
     
     public class CountdownUI : MonoBehaviour
     {
-        public List<CountdownStep> Steps;
         public TextMeshProUGUI Text;
+        public Image Image;
+        public List<CountdownStep> Steps;
+
+        private Color _initialColor;
 
         private void Awake()
         {
+            _initialColor = Image.color;
+            
             Text.gameObject.SetActive(false);
+            Image.gameObject.SetActive(false);
         }
 
         public void StartCountdown(Action<int> onStep = null, Action onComplete = null)
@@ -38,16 +45,27 @@ namespace Demos.Vehicle
         private IEnumerator Countdown(Action<int> onStep = null, Action onComplete = null)
         {
             Text.gameObject.SetActive(true);
-            
+            Image.gameObject.SetActive(true);
+            Image.color = _initialColor;
+
             for (int i = 0; i < Steps.Count; i++)
             {
                 onStep?.Invoke(i);
                 Text.SetText($"<size={Steps[i].Scale*100}%>{Steps[i].Text}");
+
+                if (i == Steps.Count - 1)
+                {
+                    var color = _initialColor;
+                    color.a = (1f - Mathf.Lerp(0f, 1f, (float)i / Steps.Count)) * _initialColor.a;
+                    Image.CrossFadeColor(color, 1f, false, true);
+                }
+                
                 yield return new WaitForSeconds(1f);
             }
             onComplete?.Invoke();
             
             Text.gameObject.SetActive(false);
+            Image.gameObject.SetActive(false);
         }
     }
 }
